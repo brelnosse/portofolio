28ecbd9c910538847234c76bccbecff62a8aca49
brelnosse <brelnosse2@gmail.com>
2026-02-17 11:09:41 +0100

    Finalized the ui of the 404 not found page

--- src/pages/NotFound.tsx
+++ src/pages/NotFound.tsx
@@ -1,10 +1,36 @@
-import { Link } from "react-router-dom";
+//@ts-ignore
+import '../assets/style/notfound.css';
+import { Link, useNavigate } from "react-router-dom";
+import Underlined from '../components/Underlined';
+import Button from '../components/Button';
+import { faArrowLeft, faGamepad } from '@fortawesome/free-solid-svg-icons';
 const NotFound:React.FC = () => {
+    const  navigate  = useNavigate();
+
     return (
-        <div className="not-found">
-            <h1>404 - Page Not Found</h1>
-            <p>Sorry, the page you are looking for does not exist.</p>
-            <Link to={'/'}>revenir en arrière</Link>
+        <div className="not-found-container">
+            <div className="not-found">
+                <img src={require('../assets/images/notfound.png')} alt='404 not found'/>
+                <span>404</span>
+                <h1>Page inexistante</h1>
+                <p>Désolé, la page que vous recherchez n'existe pas.</p>
+                <div className="links">
+                    <Button 
+                        type='light' 
+                        valueIcon={faArrowLeft} 
+                        valueText='Revenir en arrière'
+                        onClick={()=>{
+                            navigate('/');
+                        }} />
+                    <Button 
+                        type='default' 
+                        valueIcon={faGamepad} 
+                        onClick={()=> {
+                            window.open('https://sevux.netlify.app/', '_blank');
+                        }}
+                        valueText='Jouer à un jeu' />
+                </div>
+            </div>
         </div>
     );
 }
