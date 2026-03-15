3a31150391dad05e6a34e3c11a8cda7d9bb85ae9
brelnosse <brelnosse2@gmail.com>
2026-03-15 16:19:42 +0100

    Modification du positionnement de l'expérience alignement avec le contenu globale

--- src/components/Experience.tsx
+++ src/components/Experience.tsx
@@ -28,52 +28,48 @@ const Experience: React.FC<ExperienceProps> = ({enterprise, period, postTitle, l
     const getRandomColor = (index: number) => softColors[index % softColors.length];
 
     return (
-        <div 
-            className={`experience ${isVisible ? 'visible' : ''}`} 
-            ref={domRef} 
-            {...props}
-        > 
-            <div className="line">
-                <h3><MagicLink path='https://www.ovnicode.com/' text={enterprise}/></h3>
-                <span className="date">{period}</span>
-            </div>
-            
-            <div className="line" style={{fontFamily: 'Poppins'}}>
-                <h3>{postTitle}</h3>
-                <span className='location'>{location}</span>
-            </div>
-            
-            <ul className="skills_list">
-                {skills.map((el: string, i: number) => (
-                    <li 
-                        key={el + i} 
-                        style={{ '--i': i } as React.CSSProperties}
-                    >
-                        <span>
-                            {el}
-                        </span>
-                    </li>
-                ))}                
-            </ul>
+// Dans ton return :
+<div className={`experience ${isVisible ? 'visible' : ''}`} ref={domRef} {...props}> 
+    {/* Bloc Titre Entreprise */}
+    <div className="line">
+        <h3><MagicLink path='https://www.ovnicode.com/' text={enterprise}/></h3>
+        <span className="date">{period}</span>
+    </div>
+    
+    {/* Bloc Titre de Poste */}
+    <div className="line" style={{fontFamily: 'Poppins', marginTop: '10px'}}>
+        <h3 style={{fontSize: '1.1em', color: 'var(--primary-color)'}}>{postTitle}</h3>
+        <span className='location'>{location}</span>
+    </div>
+    
+    {/* Liste des Skills centrée */}
+    <ul className="skills_list">
+        {skills.map((el: string, i: number) => (
+            <li key={el + i} style={{ '--i': i } as React.CSSProperties}>
+                <span>{el}</span>
+            </li>
+        ))}                
+    </ul>
 
-            <div className="tools_container">
-                <b>Outils :</b>
-                <div className="toools">
-                    {tools && tools.map((tool: string, i: number) => (
-                        <span 
-                            className="tools_pill" 
-                            key={tool + i}
-                            style={{ 
-                                backgroundColor: getRandomColor(i),
-                                '--i': i + skills.length // Démarre après la fin des skills
-                            } as React.CSSProperties}
-                        >
-                            {tool}
-                        </span>
-                    ))} 
-                </div>
-            </div>            
-        </div>        
+    {/* Outils */}
+    <div className="tools_container">
+        <b style={{textAlign: 'center', display: 'block'}}>Outils</b>
+        <div className="toools">
+            {tools && tools.map((tool: string, i: number) => (
+                <span 
+                    className="tools_pill" 
+                    key={tool + i}
+                    style={{ 
+                        backgroundColor: getRandomColor(i),
+                        '--i': i + skills.length 
+                    } as React.CSSProperties}
+                >
+                    {tool}
+                </span>
+            ))} 
+        </div>
+    </div>            
+</div>        
     );
 }
 
