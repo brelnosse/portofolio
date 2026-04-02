ade55402b6cdba3c5874abdef736e7c1722e924e
brelnosse <brelnosse2@gmail.com>
2026-04-02 17:17:04 +0200

    Modification des icônes pour les compétences

--- src/data/skillsresumeItems.ts
+++ src/data/skillsresumeItems.ts
@@ -2,12 +2,12 @@ import { SkillsResumeCardprops } from "../types/skillsresumecard.type";
 
 export const skillsResumeItems: SkillsResumeCardprops[] = [
   {
-    image: require('../assets/images/square.png'),
+    image: require('../assets/images/star.png'),
     title: 'Développement Fullstack',
     description: 'Conception d’interfaces avec React et mise en place de logiques back-end via Node.js ou PHP.',
   },
   {
-    image: require('../assets/images/square.png'),
+    image: require('../assets/images/code.png'),
     title: 'Mobile & Desktop',
     description: 'Création d’applications multi-plateformes avec Flutter et développement d’outils Java (JavaFX, Swing).',
   },
@@ -17,7 +17,7 @@ export const skillsResumeItems: SkillsResumeCardprops[] = [
     description: 'Gestion de bases de données PostgreSQL et initiation aux langages de bas niveau (C, Assembleur).',
   },
   {
-    image: require('../assets/images/square.png'),
+    image: require('../assets/images/write.png'),
     title: 'Analyse de Besoins',
     description: 'Traduction technique des besoins utilisateurs via la modélisation UML pour structurer les projets.',
   }
