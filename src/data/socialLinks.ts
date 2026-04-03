7cb234083f14ce9c0f641778c7c6c089219fa429
brelnosse <brelnosse2@gmail.com>
2026-04-03 16:15:55 +0200

    Fix: Modification l'icône de twitter et augementation de la vitesse d'animation des socialLinks

--- src/data/socialLinks.ts
+++ src/data/socialLinks.ts
@@ -1,8 +1,8 @@
-    import { faGithub, faLinkedin, faTwitter, faFacebook } from "@fortawesome/free-brands-svg-icons";
+    import { faGithub, faLinkedin, faTwitter, faFacebook, faXTwitter } from "@fortawesome/free-brands-svg-icons";
 
     export const socialLinks = [
         { icon: faGithub, to: "https://github.com/brelnosse" },
         { icon: faLinkedin, to: "https://www.linkedin.com/in/brel-nosse-88a3a2377/" },
-        { icon: faTwitter, to: "https://x.com/BrelNosse" },
+        { icon: faXTwitter, to: "https://x.com/BrelNosse" },
         { icon: faFacebook, to: "https://www.facebook.com/profile.php?id=61568437907463" }
     ];
